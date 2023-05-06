6192c8d451181b4de9463146b08bec8569bcd215
Steve <kamaustevek78@gmail.com>
2023-05-06 18:06:11 +0300

    About section added

--- cocktail/src/pages/About.js
+++ cocktail/src/pages/About.js
@@ -6,8 +6,11 @@ const About = () => {
       <section className="section about-section">
         <h1 className="section-title">About</h1>
         <p>
-          Cocktailhaven is a web page that will let anyone create their cocktails at home.
-          It will provide ingredients and instructions on how to make the cocktails.
+          Welcome to our cocktail recipe website!! We are dedicated to bringing you the best and most delicious recipes from around the world. Our team of expert mixologists has carefully curated a collection of classic and modern cocktails. Whether you are looking for a refreshing summer cocktail, a cozy winter warmer, or a sophisticated cocktail to impress your guests, we've got you covered. From the classic Margarita to the trendy Aperol Spritz, our recipes are sure to tantalize your taste buds.
+
+          In addition to our cocktail recipes, we also provide tips and tricks for making the perfect cocktail, including how to properly muddle herbs, shake cocktails, and garnish your drinks like a pro.
+
+          So grab your shaker and let's get mixing! Cheers!
         </p>
       </section>
     </div>
