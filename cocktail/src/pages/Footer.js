046fc4fc4fa048463e6f4c93d1feed2c5bc34216
Wangari Samantha <samanthanyams5@gmail.com>
2023-05-06 21:43:26 +0300

    copyright

--- cocktail/src/pages/Footer.js
+++ cocktail/src/pages/Footer.js
@@ -4,22 +4,25 @@ import {
     faFacebook,
     faTwitter,
     faInstagram
-  } from "@fortawesome/free-brands-svg-icons";
+} from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
     return (
-        <>
-      <a href="https://www.facebook.com/"
-        className="facebook social">
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
-      <a href="https://www.twitter.com/" className="twitter social">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
-      </a>
-      <a href="https://www.instagram.com/"
-        className="instagram social">
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
-    </>
+        <footer className="footer">
+            <p>&copy; 2021 Cocktail Reviews. All rights reserved.</p>
+            <div className="social-media">
+                <a href="https://www.facebook.com/" className="facebook social">
+                    <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="https://www.twitter.com/" className="twitter social">
+                    <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a href="https://www.instagram.com/" className="instagram social">
+                    <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+            </div>
+        </footer>
     );
 };
+
 export default Footer;
