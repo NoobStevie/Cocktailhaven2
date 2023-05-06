674fbd2ff63f3d85e04f437726f5715b868b7706
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-05-06 19:11:14 +0300

    added styling to the navbar

--- cocktail/src/components/Navbar.js
+++ cocktail/src/components/Navbar.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Logo from "./logo-color.png";
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
-        <NavLink to="/">
-          <div className="logo">
-            <NavLink to="/">
-              <img src={Logo} alt="Logo" className="logo-img" />
-            </NavLink>
-          </div>
-        </NavLink>
         <ul className="nav-links">
           <li>
             <NavLink to="/" className="nav-link">Home</NavLink>
           </li>
           <li>
             <NavLink to="/" className="nav-link">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="nav-link">Reviews</NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="nav-link">Contact</NavLink>
           </li>
         </ul>
       </div>
