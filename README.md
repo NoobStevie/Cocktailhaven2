7b503f5293bcdafe51bb0bdb42acd5b304c78189
Steve <kamaustevek78@gmail.com>
2023-05-06 16:08:13 +0300

    App.js and App.css Done

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 ## Author name 
 1. [Samantha Wangari](https://github.com/WANGAR1)
-2. [Denis Omondi](https://github.com/Denisomondi)
-3. [Stephanie Wanja](https://github.com/WanjaNjunge)
-4. [Steve Kariuki](https://github.com/NoobStevie)
+2. [Stephanie Wanja](https://github.com/WanjaNjunge)
+3. [Steve Kariuki](https://github.com/NoobStevie)
 
 ## Description of project
 In this end of Phase2 project we created a mixologist's dream. Using TheCocktailDB we created Cocktailhaven everyone's go to web page for everything 🍹COCKTAILS🍹.
--- cocktail/src/App.js
+++ cocktail/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './App.css'
+
+import Filter from './components/Filter';
+import RandomCocktail from './components/RandomCocktail';
+import NavBar from './components/Navbar';
+import SearchContainer from './components/SearchContainer';
+import About from './pages/About';
+import CocktailReviews from './pages/CocktailReviews'
+
+const API_KEY = '1';
 
 function App() {
+  const [cocktails, setCocktails] = useState([]);
+  const [reviewsUpdate, setReviewsUpdate] = useState(false);
+
+  function updateReviews() {
+    setReviewsUpdate(!reviewsUpdate);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+
+      <div>
+        <Filter setCocktails={setCocktails} apiKey={API_KEY} />
+        <ul className="cocktail-grid">
+          {cocktails.map(cocktail => (
+            <li key={cocktail.idDrink}>
+              <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+              {cocktail.strDrink}
+            </li>
+    ))}
+  </ul>
+</div>
+
+        <div>
+          <RandomCocktail />
+          <SearchContainer />
+          <About />
+           
+        
+        </div>
+        <div>
+           <h1>Reviews</h1>
+           <CocktailReviews cocktailID={1} updateReviews={updateReviews} />
+      </div>
     </div>
   );
 }
